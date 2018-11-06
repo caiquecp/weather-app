a59e5922f7716a451314d151061b1b8d76cfafc6
Caique Cunha Pereira <caique_cp@hotmail.com>
2018-11-06 19:14:43 -0200

    Play with async

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -1,7 +1,19 @@
 console.log('Starting app');
 
-setTimeout(() => console.log('Inside of callback'), 2000);
+function msleep(n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
 
-setTimeout(() => console.log('Second setTimeOut', 0));
+function sleep(n) {
+    msleep(n * 1000);
+}
+
+setTimeout(() => console.log('Inside of callback'), 2000);
+sleep(5);
+
+// the setTimeout callback will only be executed after the sleep 
+// of 5s seconds even the time set being 2s because the callback 
+// stack is only executed (the callback go to the call stack) 
+// when the call stack is empty; weird
 
 console.log('Finishing up');
